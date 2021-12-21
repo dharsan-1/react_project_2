1ccdbc3d98d297ff8de82c2cd510df5ed41ebc2e
dharsan-1 <dharsanramakrishnan@gmail.com>
2021-12-21 09:37:25 +0000

    added new code

---
+++ index.js
@@ -0,0 +1,22 @@
+const element = (
+  <div className="back">
+    <h1 className="h-1">congratulation</h1>
+    <div className="back-2">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="img-1"
+      />
+      <h1>kiran v</h1>
+      <p>
+        Want to quickly review some of the concepts you’ve been learning? Take a
+        look at the Cheat Sheets.
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="img-2"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
